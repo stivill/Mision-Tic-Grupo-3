10bad5331d2f67500b5f059601bda911c6519a9b
stivill <stivillrivera@hotmail.com>
2021-09-29 16:02:35 -0500

    backend

---
+++ src/main/java/com/misiontic/logica/Producto.java
@@ -0,0 +1,201 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.misiontic.logica;
+
+import com.misiontic.persistencia.ConexionBD;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author o
+ */
+public class Producto {
+    
+    private int codigo;
+    private String nombre;
+    private int cantidad;
+    private String categoria;
+    private double precio;
+
+    
+    public Producto(){}
+    
+   
+    public Producto getProducto(int codigo)throws SQLException{
+        this.codigo = codigo;
+        return this.getProducto();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+    
+    
+    
+    public void llenarProducto(int codigo, String nombre, int cantidad, String categoria, double precio){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+        this.precio = precio;        
+    }
+    
+    public boolean guardarProducto() {
+        System.out.println("bien");
+        ConexionBD conexion = new ConexionBD();
+        String sentencia = "INSERT INTO producto(nombre,categoria,cantidad,precio)"
+                +"VALUES('"+this.nombre+"','"+this.categoria+"',"+this.cantidad+","+this.precio+");";
+        if (conexion.setAutoCommit(false)) {
+            if (conexion.insertDB(sentencia)) {
+                conexion.CommitBD();
+                conexion.cerrarConexion();
+                return true;
+            } else {
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        } else {
+            conexion.cerrarConexion();
+            return false;
+        }
+        
+   
+    }
+    
+    public boolean borrarProducto(int codigo){
+        ConexionBD conexion = new ConexionBD();
+        String sentencia = "DELETE FROM producto WHERE id="+ codigo +";";
+        if (conexion.setAutoCommit(false)) {
+            if (conexion.actualizartDB(sentencia)) {
+                conexion.CommitBD();
+                conexion.cerrarConexion();
+                return true;
+            } else {
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        } else {
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+    
+    public boolean actualizarProducto() {
+        ConexionBD conexion = new ConexionBD();
+        String sentencia = "UPDATE producto SET nombre='"
+                +this.nombre+"',categoria='"+this.categoria
+                +"',cantidad="+this.cantidad+",precio="
+                +this.precio+" WHERE codigo="+this.codigo+";";
+        if (conexion.setAutoCommit(false)) {
+            if (conexion.actualizartDB(sentencia)) {
+                conexion.CommitBD();
+                conexion.cerrarConexion();
+                return true;
+            } else {
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        } else {
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+    
+    public List<Producto> listarProductos() {
+        List<Producto> listaProductos = new ArrayList();
+        ConexionBD conexion = new ConexionBD();
+        String sentencia = "select * from producto";
+        try {
+            ResultSet rs = conexion.consultarBD(sentencia);
+            Producto p;
+            while (rs.next()) {
+                p=new Producto();
+                p.setCodigo(rs.getInt("codigo"));
+                p.setCantidad(rs.getInt("cantidad"));
+                p.setNombre(rs.getString("nombre"));
+                p.setCategoria(rs.getString("categoria"));
+                p.setPrecio(rs.getDouble("precio"));
+                listaProductos.add(p);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error"+ex.getMessage());
+        }finally{
+            conexion.cerrarConexion();
+        }
+        return listaProductos;
+    }
+    
+    public Producto getProducto() throws SQLException {
+        ConexionBD conexion = new ConexionBD();
+        String sql = "select * from producto where codigo='" + this.codigo + "'";
+        ResultSet rs = conexion.consultarBD(sql);
+        if (rs.next()) {
+            this.codigo = rs.getInt("codigo");
+            this.nombre = rs.getString("nombre");
+            this.categoria = rs.getString("categoria");
+            this.precio = rs.getDouble("precio");
+            this.cantidad = rs.getInt("cantidad");
+            return this;
+            
+        } else{
+            conexion.cerrarConexion();
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", nombre=" + nombre + ", cantidad=" + cantidad + ", categoria=" + categoria + ", precio=" + precio + '}';
+    }
+    
+    
+
+}
---
+++ src/main/java/com/misiontic/persistencia/ConexionBD.java
@@ -0,0 +1,158 @@
+
+package com.misiontic.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ConexionBD {
+  
+    
+    private String DB_driver = "";
+    private String url = "";
+    private String db = "";
+    private String host = "";
+    private String username = "";
+    private String password = "";
+    private Connection con = null;
+    private Statement stmt = null;
+    private PreparedStatement pstmt = null;
+    private ResultSet rs = null;
+    private boolean local;
+    
+    
+    public ConexionBD(){
+       DB_driver = "com.mysql.jdbc.Driver" ;
+       host = "localhost:3306";
+       url = "jdbc:mysql://" + host + "/" + db;
+       username = "root";
+       password = "admin";
+       
+        try {
+            Class.forName(DB_driver);
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Conexión Exitosa...");
+        } catch (Exception ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+              
+    }
+    
+    //Metodo que retorna la conexion
+    public Connection getConnection(){
+        return con;
+    }
+    
+    //Metodo para cerrar la conexión
+    public void closeConnection (Connection con){
+        if(con != null){
+            try {
+                con.close();
+            } catch (Exception ex) {
+                Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    //Metodo que permite guardar lo obtenido de una consulta ResultSet 
+    public ResultSet consultarBD(String sentencia){
+        try {
+            stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stmt.executeQuery(sentencia);
+        }  catch (Exception ex){            
+        }
+        return rs;
+    }
+    
+    //Metodo que realiza un INSERT y devuelve TRUE si la operacion es exitosa
+    public boolean insertDB(String sentencia){
+       System.out.println("Registro Exitoso...");
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sentencia);            
+        } catch (SQLException | RuntimeException sqlex) {
+            System.out.println("ERROR RUTINA: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+    
+    //Metodo que permite ELIMINAR un registro.
+    public boolean borrartDB(String sentencia){
+       System.out.println("Registro Eliminado Exitosamente...");
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sentencia);            
+        } catch (SQLException | RuntimeException sqlex) {
+            System.out.println("ERROR RUTINA: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+    
+    //Metodo que permite ACTUALIZAR un registro.
+    public boolean actualizartDB(String sentencia){
+       System.out.println("Registro Modificado Exitosamente...");
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sentencia);            
+        } catch (SQLException | RuntimeException sqlex) {
+            System.out.println("ERROR RUTINA: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+    
+    //Metodo que permite ELIMINAR un registro.
+    public boolean setAutoCommit(boolean parametro){
+       
+        try {
+           con.setAutoCommit(parametro);        
+        } catch (SQLException  sqlex) {
+            System.out.println("ERROR al configurar el AutoCommit: " + sqlex);
+            return false;
+        }
+        return true;
+    }
+    
+    public void cerrarConexion(){
+        closeConnection(con);
+    }
+    
+    //Metodo para realizar el commit
+    public boolean CommitBD(){
+       
+        try {
+           con.commit();
+           return true;
+        } catch (SQLException  sqlex) {
+            System.out.println("ERROR al realizar Commit: " + sqlex);
+            return false;
+        }
+    }
+    
+    //Metodo para realizar el rollback 
+    public boolean rollbackBD(){
+       
+        try {
+           con.rollback();
+           return true;
+        } catch (SQLException  sqlex) {
+            System.out.println("ERROR al realizar rollback: " + sqlex);
+            return false;
+        }
+    }
+    
+    //Metodo Mian para ejecutar y cerrar conexion
+    public static void main(String[] args) {
+        ConexionBD b = new ConexionBD();
+        b.cerrarConexion();
+    }
+    
+}
